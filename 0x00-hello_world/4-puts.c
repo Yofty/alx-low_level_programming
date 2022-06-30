904cd4b5485198fae1a92816e1c43b18bc0b1306
Yofty <106748943+Yofty@users.noreply.github.com>
2022-06-30 04:35:27 -0700

    betty documentation style corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include  <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * print string 
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char str1[]="\"Programming is like building a multilingual puzzle";
