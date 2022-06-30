274831458575a4a72f03da7c2ba7f043f3d65af7
Yofty <106748943+Yofty@users.noreply.github.com>
2022-06-30 05:36:51 -0700

    puts C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,8 +9,6 @@
  */
 int main(void)
 {
-	char str1[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(str1);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
