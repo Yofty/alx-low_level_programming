36a71b4cb00cde9718fada9c356707588e965f76
Yofty <106748943+Yofty@users.noreply.github.com>
2022-06-30 10:44:25 -0700

    print to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * print to the standard error
@@ -12,6 +11,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		
 	return (1);
 }
