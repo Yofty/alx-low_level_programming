8783c1cbea176d63f20bd34350e0a5381c8b9412
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-02 12:01:25 -0700

    combine 2 set of number

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = x + 1 ; y < 10 ; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (x < 8 || y < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
