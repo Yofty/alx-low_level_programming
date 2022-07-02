da5b31d0589e653a639d0a0c64b081129203b36a
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-02 12:53:04 -0700

    combination of 2 double digit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z, a;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = x + 1 ; y < 10 ; y++)
+		{
+			for (z = y + 1 ; z < 10 ; z++)
+			{
+				for (a = z + 1; a < 10 ;a++)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(' ');
+					putchar(z + '0');
+					putchar(a + '0');
+					
+					if(x < 6 || y < 7 || z < 8 || a < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
