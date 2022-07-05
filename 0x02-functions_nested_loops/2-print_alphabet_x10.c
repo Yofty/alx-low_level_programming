1ce1555ed46b1904c01a83798c9b002a1d2d03bc
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-05 12:19:56 -0700

    print 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lower case 10x
+ *
+ * return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
