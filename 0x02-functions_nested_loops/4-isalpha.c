ef7452cf6f32f03e660916bdffb6eaaeeb1b6f0f
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-05 13:32:30 -0700

    identifies letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - hecks for alphabetic character
+ * @c - thc input from user
+ *
+ * Return: 1 (for letter) or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
