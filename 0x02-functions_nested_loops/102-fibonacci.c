b91d2f2dc3f6aa2de019c04ec2026e3fc8c6b6ad
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-06 13:04:01 -0700

    sun of fibonnachi number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	x = 1;
 	y = 2;
 
-	for (a = 0 ; a < 50 ; a++)
+	for (a = 1 ; a <= 50 ; a++)
 	{
 		z = x + y;
 		x = y;
 		z = y;
 
 		printf("%d", z);
-		if (a < 49)
+		if (x != 20365011074)
 			printf(", ");
 	}
 	putchar('\n');
