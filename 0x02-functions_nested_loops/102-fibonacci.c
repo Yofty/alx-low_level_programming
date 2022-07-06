eb0cb3a3a9ec05d63269de2e6bfea2cd4b9dbde9
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-06 12:53:31 -0700

    sum of fibonnachi numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - peirnts the 1st 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	x = 1;
+	y = 2;
+
+	for (a = 0 ; a < 50 ; a++)
+	{
+		z = x + y;
+		x = y;
+		z = y;
+
+		printf("%d", z);
+		if (a < 49)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
+}
