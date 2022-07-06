abb7807da3d928bdd89f839c56e4710ae506d132
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-05 21:17:35 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * starts with 0
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = 0 ; y < 10 ; y++)
+		{
+			if ((x * y) < 10)
+			{
+				_putchar(' ');
+				_putchar((x * y) + '0');
+			}
+			else
+			{
+				_putchar(((x * y) / 10) + '0');
+				_putchar(((x * y) % 10) + '0');
+			}
+
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
