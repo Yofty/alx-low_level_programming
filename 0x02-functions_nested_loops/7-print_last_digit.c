e44772da76a105d3f928063cf1b3105c98355bce
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-05 20:22:05 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,16 @@
  * print_last_digit - prints the last digit of a number
  * @n : n is an intiger input
  *
- * Return: 0
+ * Return: x
  */
 int print_last_digit(int n)
 {
 	int x = n % 10;
 
 	if (n < 0)
+	{
 		x = x * -1;
-
+	}
 	_putchar(x +'0');
-	return (0);
+	return (x);
 }
