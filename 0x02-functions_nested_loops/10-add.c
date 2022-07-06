68acab0af7845dfe63f87be4640a9a3b11eb9146
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-06 05:17:46 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -adds two integers and returns the result
+ * @a : a is the integer user inter
+ * @b : b is the integer user inter 
+ *
+ * Return: int (r)
+ */
+int add(int a, int b)
+{
+	int r = (a + b);
+
+	return (r);
+}
