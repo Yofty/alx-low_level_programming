65db399d557dc1526885f08aca47d22af69db05e
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-06 12:06:03 -0700

    n time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,33 @@
 #include "main.h"
 /**
  * print_times_table - prints the n times table
- * starts with 0
  * @n : n is the integer user inters
- *
  * Return: void
  */
 void print_times_table(int n)
 {
-	int x, y, r;
+	int x, y;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (x = 0 ; x <= n ; x++)
 		{
 			for (y = 0 ; y <= n ; y++)
 			{
-				r = x * y;
-				if (y == 0)
-				{
-					_putchar(r + '0');
-				}
-				else if (r < 10)
+				if ((x * y) < 100 && y > 0)
 				{
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(r + '0');
+					if ((x * y) < 10 && y > 0)
+						_putchar(' ');
+					else
+						_putchar(((x * y) / 10) + '0');
 				}
-				else if (r < 100)
+				else if ((x * y) >= 100 && y > 0)
 				{
-					_putchar(' ');
-					_putchar((r / 10) + '0');
-					_putchar((r % 10) + '0');
+					_putchar(((x * y) / 100) + '0');
+					_putchar((((x * y) % 100) / 10) + '0');
 				}
-				else
-				{
-					_putchar((r / 100) + '0');
-					_putchar(((r % 100) / 10) + '0');
-					_putchar((r % 10) + '0');
-				}
-
+				_putchar(((x * y) % 10) + '0');
 				if (y < n)
 				{
 					_putchar(',');
