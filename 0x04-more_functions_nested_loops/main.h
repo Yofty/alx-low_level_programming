df38edaee4572fabb388ffbd06fa436fb6dab95e
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-07 11:42:52 -0700

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -12,5 +13,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _isdigit(int c);
-#endif 
+#endif
