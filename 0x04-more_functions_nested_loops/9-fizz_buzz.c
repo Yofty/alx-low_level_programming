367eff06a6c5a19d44a31e8089d085a97ed9a53d
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-08 08:07:30 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1 ; x <= 100 ; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
