8d21bf393f7448d28ae7d6b08ccdee2a79000786
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-08 03:57:32 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
