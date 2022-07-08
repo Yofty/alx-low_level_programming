2441ecfef264589ceba5ba05597c5c6b11c8f73b
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-08 06:49:24 -0700

    draw strait line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n : n is the int user enters
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0 ; x < n ; x++)
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
