b8a96e9c8bdead57e158a591003cceb21e99b683
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-11 11:18:59 -0700

    print n numbers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,9 @@ void print_array(int *a, int n)
 
 	for (x = 0 ; x < n ; x++)
 	{
+		printf("%d", a[x]);
 		if (x < n - 1)
-			printf("%d, ", a[x]);
-		else
-			printf("%d", a[x]);
+			printf(", ");
 	}
 	_putchar('\n');
 }
