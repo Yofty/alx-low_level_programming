ec325746aa8c47aac3b11e027ff9c82bfe5afadb
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-11 08:23:10 -0700

    swaps int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
+ * swap_int - swaps the values of two integers
+ * @a : a is pointer to int to be swaped
+ * @b : b is poinet to int to be swaped
  *
- *
- *
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
