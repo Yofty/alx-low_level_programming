f2e6d2bd9ddbd45ef30d7a30586427e85f90df2f
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-11 10:49:07 -0700

    every other integer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str - str is the pointer ro be updated
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int x = 0;
+
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		if ( x % 2 == 0)
+			_putchar(str[x]);
+	}
+	putchar('\n');
+}
