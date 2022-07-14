787e36f8b38965b6efc6e055fa3d0a3971e072f0
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-14 15:47:23 -0700

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,6 @@
  * reverse_array - reverses the content of an array of integers
  * @a : a is the int to be updated
  * @n : n is the number of elements of the array
- *
  * Return: nothing
  */
 void reverse_array(int *a, int n)
@@ -12,7 +11,6 @@ void reverse_array(int *a, int n)
 
 	x = a;
 	y = a;
-
 	for (i = 0; i < n - 1; i++)
 	{
 		y++;
@@ -22,7 +20,6 @@ void reverse_array(int *a, int n)
 		z = *y;
 		*y = *x;
 		*x = z;
-
 		x++;
 		y--;
 	}
