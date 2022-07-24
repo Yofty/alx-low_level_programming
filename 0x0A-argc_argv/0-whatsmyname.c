a4cac45dddad8a68ffab688a5ce08d83876b5dbf
Yofty <106748943+Yofty@users.noreply.github.com>
2022-07-24 02:22:47 -0700

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * main - print the name of the program
+ * @argc : the number of arguments used
+ * @argv : the string of arguments
  *
- *
+ * Return : Always 0
  */
 int main(int argc, char* argv[])
 {
 	if (argc >= 1)
 		printf("%s\n", argv[0]);
+
 	return (0);
 }
