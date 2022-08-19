e6ca1de6c61c2e8a30ee8c0616cff1dc69ef26c3
Yofty <106748943+Yofty@users.noreply.github.com>
2022-08-19 14:36:35 -0700

    bonus

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: int
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *a = (char *) & x;
+
+	return (*c);
+}
