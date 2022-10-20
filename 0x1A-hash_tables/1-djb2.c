6e70671d8496ac1800af2e0544d5ab1a1886b3f0
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-20 13:25:40 -0700

    correction

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
  * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
+ * @str : The string to hash.
  *
  * Return: The calculated hash.
  */
@@ -11,9 +11,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	
+
 	return (hash);
 }
