1229beeadc3e2e52ab658090d5145104691dff75
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-20 13:29:23 -0700

    correction

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -22,6 +22,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	node = ht->array[index];
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
-	
+
 	return ((node == NULL) ? NULL : node->value);
 }
