2db31ee348fead229f92320dbc825de844dbb73d
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-22 04:29:24 -0700

    correction6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * sum_dlistint - Sums all the data of a dlistint_t list.
  * @head: The head of the dlistint_t list.
- * 
+ *
  * Return: The sum of all the data.
  */
 int sum_dlistint(dlistint_t *head)
